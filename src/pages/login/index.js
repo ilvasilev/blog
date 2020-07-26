4abda332694850788ca6fa1279c4dabbf6a18370
Ilian Vasilev <ivasilev@gmail.com>
2020-07-26 15:47:33 +0300

    Implementing proto login and register

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,26 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Wrapper from '../../components/wrapper'
 import Title from '../../components/title'
 import Input from '../../components/input'
 
-const LoginPage = () => {
+class LoginPage extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  onChange = (event, type) => {
+    const newState = {}
+    newState[type] = event.value
+
+    this.setState(newState)
+  }
+
+  render() {
+    const {email,
+    password} = this.state
+    
     return (
         <Wrapper>
           <Title title={'Login page'} />
           <Input
-          value='Enter your e-mail here'
-          onChange={() => {}}
+          value={email}
+          onChange={(e) => this.onChange(e, 'email')}
           label='E-mail'
           id='email'
           />
           <Input
-          value='Enter your password here'
-          onChange={() => {}}
+          value={password}
+          onChange={(e) => this.onChange(e, 'password')}
           label='Password'
           id='password'
           />
         </Wrapper>
     )
+  }
 }
 
 export default LoginPage
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,13 +1,54 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Wrapper from '../../components/wrapper'
 import Title from '../../components/title'
+import Input from '../../components/input'
 
-const RegisterPage = () => {
+class LoginPage extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      password: '',
+      rePassword: ''
+    }
+  }
+
+  onChange = (event, type) => {
+    const newState = {}
+    newState[type] = event.value
+
+    this.setState(newState)
+  }
+
+  render() {
+    const {email, password, rePassword} = this.state
+    
     return (
         <Wrapper>
-          <Title title={'Register page'} />          
+          <Title title={'Register page'} />
+          <Input
+          value={email}
+          onChange={(e) => this.onChange(e, 'email')}
+          label='E-mail'
+          id='email'
+          />
+          <Input
+          value={password}
+          onChange={(e) => this.onChange(e, 'password')}
+          label='Password'
+          id='password'
+          />
+          <Input
+          value={rePassword}
+          onChange={(e) => this.onChange(e, 'rePassword')}
+          label='Re Password'
+          id='re-password'
+          />
         </Wrapper>
     )
+  }
 }
 
-export default RegisterPage
+export default LoginPage
